2041def49aeabfd5ec0106d2cdc447220bfc7bab
mdrashedulislam22 <121747355+mdrashedulislam22@users.noreply.github.com>
2023-04-08 12:01:18 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector(".Register-now").style.display = "none"
+setTimeout(submit3,5000)
+function submit3 (){
+    document.querySelector(".Register-now").style.display = "block";
+    document.querySelector("#section1").style.display = "none"
+}
+     
+    
+
